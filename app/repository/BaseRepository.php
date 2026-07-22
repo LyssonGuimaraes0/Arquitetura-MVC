b544cd3e31d1400ea1e143b86c54c6ecd292fd7f
Alysson Andrade <alyssonandrade186@gmail.com>
2026-07-22 00:38:35 -0300

    Feat: função Select base

---
+++ app/repository/BaseRepository.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\repository;
+
+use App\Database\Database;
+use PDO;
+
+class BaseRepository extends Database
+{
+
+    //Função Para Montar Insert 
+    protected function insert(string $table, array $query) {}
+
+    //Função Para Montar Select 
+    protected function select(string $table, array $columns, array $joins = [], array $where = [], array $orderBy = [], bool $fetchAll = false)
+    {
+        $sql = "SELECT " . implode(', ', $columns) . " FROM $table";
+        $values = [];
+
+        // Verifica se possui JOIN
+        if (!empty($joins)) {
+
+            foreach ($joins as $join) {
+
+                $type = strtoupper($join['type']);
+
+                $allowedJoins = [
+                    'INNER',
+                    'LEFT',
+                    'RIGHT',
+                    'FULL'
+                ];
+
+                if (!in_array($type, $allowedJoins)) {
+                    throw new \Exception("Tipo de JOIN inválido.");
+                }
+
+                $sql .= " {$type} JOIN {$join['table']}";
+
+                $sql .= " ON {$join['on']['column']} {$join['on']['operator']} {$join['on']['reference']}";
+            }
+        }
+
+        //Verifica se Tem parametro
+        if (!empty($where)) {
+            $conditions = [];
+
+            $operator = $join['type'];
+
+            $allowedoperator = ['>=', '<=', '!=', '<>', '>', '<', 'LIKE', 'like', '='];
+
+            if (!in_array($operator, $allowedoperator)) {
+                throw new \Exception("Tipo de JOIN inválido.");
+            }
+
+            //Monta Where $condition $operador ?
+            foreach ($where as $condition) {
+                $conditions[] = "{$condition['column']} {$operator} ?";
+                $values[] = $condition['value'];
+            }
+
+            $sql .= " WHERE " . implode(" AND ", $conditions);
+        }
+
+        // Verifica se possui ORDER BY
+        if (!empty($orderBy)) {
+            $sql .= " ORDER BY ";
+            $orders = [];
+
+            //Monta Where $order $direction
+            foreach ($orderBy as $order) {
+
+                $direction = strtoupper($order['direction']);
+
+                if (!in_array($direction, ['ASC', 'DESC'])) {
+                    throw new \Exception("Direção do ORDER BY inválida.");
+                }
+
+                $orders[] = "{$order['column']} {$direction}";
+            }
+            $sql .= implode(", ", $orders);
+        }
+
+        $pdo = self::connection();
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute($values);
+
+        if ($fetchAll != false) {
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        } else {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+    }
+}
